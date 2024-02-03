c1a857d3455ad7227af8ca5185c8cb8e9844dde1
Faceless <146445263+TheRealFaceless@users.noreply.github.com>
2024-02-03 08:28:58 +0100

    slight gradle change😊

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,15 +27,18 @@ tasks.withType(JavaCompile::class.java) {
     options.encoding = "UTF-8"
 }
 
+val paths = listOf(
+    "C:\\Users\\Faceless\\Desktop\\Servers\\Paper 1.20.4\\plugins",
+    "C:\\Users\\kaden\\OneDrive\\Documents\\[1] My Files\\Development\\Test Server (Paper)\\plugins"
+)
 
-val path = "C:\\Users\\Faceless\\Desktop\\Servers\\Paper 1.20.4\\plugins"
-
-if (file(path).exists()) {
-    tasks.jar {
-        destinationDirectory.set(file(path))
-    }
-} else {
-    tasks.jar {
-        destinationDirectory.set(file("C:\\Users\\kaden\\OneDrive\\Documents\\[1] My Files\\Development\\Test Server (Paper)\\plugins"))
+tasks.jar {
+    doLast {
+        paths.forEach { path ->
+            if (file(path).exists()) {
+                destinationDirectory.set(file(path))
+                return@forEach
+            }
+        }
     }
 }
